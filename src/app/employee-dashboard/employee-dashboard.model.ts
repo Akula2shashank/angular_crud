fb641b1073bc0b37bf9798f793927b6a6a258bcc
Shashank <shashankakula2000@gmail.com>
2023-11-16 11:30:53 +0530

    angular crud operations

---
+++ src/app/employee-dashboard/employee-dashboard.model.ts
@@ -0,0 +1,8 @@
+export class EmployeeModel{
+   id : number = 0;
+   email : string = '';
+   password : string = '';
+   // email : string  = '';
+   // mobile:string = '';
+   // Salary : string = ''
+}
